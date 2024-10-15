5d91e226def69825022492b45f9b6d9af9fc38a0
Lan Ngai <b00159127@mytudublin.ie>
2024-10-15 19:29:20 +0100

    Challenge2: The fetch game challenge
    Where the player is a farmer telling the dog to fetch random balls that fall from above.

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float time = 1.0f;
+    private float timer = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= time){//if statement to stop player from spamming
+            // On spacebar press, send dog
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                dogSpawner();
+                timer = 0;
+            }
+        }
+    }
+
+    void dogSpawner (){
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+    }
+}
